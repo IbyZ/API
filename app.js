d99eb3ec965d8e47f17f62c04c2d49047217171e
Franco Velazquez <francoibrahim9@gmail.com>
2022-11-25 22:41:28 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
+let pagina = 1;
+const btnAnterior=document.getElementById('btnAnterior');
+const btnSiguiente=document.getElementById('btnSiguiente');
+btnSiguiente.addEventListener('click', () => {
+    if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
 const cargarPeliculas = async() => {
-	const respuesta = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473");
-    console.log(respuesta);
+    try{
+	    const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&page=${pagina}`);
+        console.log(respuesta);
+        if (respuesta.type != "adult"){
+            if (respuesta.status === 200){
+                const datos = await respuesta.json();
+                let peliculas = "";
+                datos.results.forEach(pelicula => {
+                    peliculas += `
+                        <div class="pelicula">
+                            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                            <h3 class="titulo">${pelicula.title}</h3>
+                        </div>
+                    `;
+                });
+                document.getElementById("contenedor").innerHTML = peliculas;
+            }
+        }else if (respuesta.type === "adult"){
+            if (respuesta.status === 200){
+                const datos = await respuesta.json();
+                let peliculas = "";
+                datos.results.forEach(pelicula => {
+                    peliculas += `
+                        <div class="pelicula">
+                            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                            <h3 class="titulo"> (Adultos) ${pelicula.title}</h3>
+                        </div>
+                    `;
+                });
+                document.getElementById("contenedor").innerHTML = peliculas;
+            }
+        }
+    } catch(error){
+        console.log(error);
+    }
 }
 cargarPeliculas();
